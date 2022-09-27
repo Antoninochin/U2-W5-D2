97ead9dfa82ac80c019c6acc8c6a6542c882d402
Antoninochin <antonino_chindemi@virgilio.it>
2022-09-27 16:54:42 +0200

    compito

---
+++ Compito/JS/script.js
@@ -0,0 +1,36 @@
+
+
+/*function budget(){
+    let budget = 100
+    alert(`Il tuo budget è di ${budget}€`)
+    document.getElementById('seeBudget').innerHTML = `${budget}€`
+}
+let budget2 = prompt('Chiedimi fin dove sei arrivato con il budget')*/
+
+function view(){
+    let budget = document.getElementById('budget-select').value
+    let screen = document.getElementById('screen')
+
+    switch (budget){
+        case 'budgetAttuale':
+        screen.innerHTML = 'Il tuo budget è di 100€'
+        break;
+
+        case 'budgetSuccessivo':
+        screen.innerHTML = 'Metà del budget è esaurito'
+        break;
+
+        case 'budgetFinale':
+        screen.innerHTML = 'Sei quasi alla fine del budget'
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
